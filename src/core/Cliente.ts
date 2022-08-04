aa11a6aba265f3cbb2c5566ddb4d4c57bd714956
Jeovane dos Santos Santos <jeovane.ssantos@outlook.com>
2022-08-04 18:14:27 -0300

    chore: settings

--- src/core/Cliente.ts
+++ src/core/Cliente.ts
@@ -1,24 +1,24 @@
 export default class Cliente{
-    #id: string
-    #nome: string
-    #idade: number
+    _id: string
+    _nome: string
+    _idade: number
 
     constructor(nome: string, idade: number, id: string=null){
 
-        this.#nome = nome
-        this.#idade = idade
-        this.#id = id
+        this._nome = nome
+        this._idade = idade
+        this._id = id
     }
     static vazio(){
         return new Cliente("", 0)
     }
     get id(){
-        return this.#id
+        return this._id
     }
     get nome(){
-        return this.#nome
+        return this._nome
     }
     get idade(){
-        return this.#idade
+        return this._idade
     }
 }
